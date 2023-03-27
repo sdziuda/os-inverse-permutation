ffed304bbd8c7890f6d7e2fbddd2030e942b7f3d
sdziuda <szymondziuda95@gmail.com>
2023-03-27 20:26:04 +0200

    Improved style

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -6,9 +6,9 @@ global inverse_permutation
 inverse_permutation:
         cmp     rdi, 0x0                ; porównujemy n z 0
         jle     .incorrect              ; jeśli n <= 0, to przechodzimy do etykiety .incorrect
-        mov     rcx, 0x80000001         ; ustawiamy rax na największą liczbę 32-bitową (ze znakiem) + 1 (2^31 + 1)
-        cmp     rdi, rcx                ; porównujemy n z 2^31 + 1 (największą liczbą dla której n ma sens)
-        jge     .incorrect              ; jeśli n >= 2^31 + 1, to przechodzimy do etykiety .incorrect
+        mov     rcx, 0x80000000         ; ustawiamy rcx na największą liczbę 32-bitową (ze znakiem -> 2^31)
+        cmp     rdi, rcx                ; porównujemy n z 2^31 (największą liczbą dla której n ma sens)
+        jg      .incorrect              ; jeśli n > 2^31, to przechodzimy do etykiety .incorrect
         mov     r8, 0x0                 ; ustawiamy r8 na 0 (indeks w tablicy)
 .loop_begin:
         movsxd  rcx, DWORD [rsi+r8*4]   ; wczytujemy wartość z tablicy
@@ -17,7 +17,7 @@ inverse_permutation:
         cmp     rcx, rdi                ; porównujemy wartość w tablicy z n
         jge     .incorrect              ; jeśli jest >= n, to przechodzimy do etykiety .incorrect
         inc     r8                      ; zwiększamy indeks
-        cmp     r8, rdi                 ; porównujemy indeks (rax) z n (rdi)
+        cmp     r8, rdi                 ; porównujemy indeks (r8) z n (rdi)
         jl      .loop_begin             ; jeśli indeks < n, to przechodzimy do etykiety .loop_begin (kontynuujemy pętlę)
         mov     rax, 0x1                ; jeśli przeszliśmy przez to wszystko, to dane są poprawne, zatem rax = 1 (true)
         ret                             ; i zwracamy true
